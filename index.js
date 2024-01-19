ec4b3c3cef91c610bdc1a6c422f8b18c8c4a3574
prafulla <prafullnalawade1121@gmail.com>
2024-01-19 19:42:41 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Starting the server...");
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import Connection from './database/db.js';
+import DefaultData from './default.js';
+import Route from './routes/route.js' ;
+
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use('/', Route);
+
+const PORT = process.env.PORT || 8000;
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const URL = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@cluster0.d0ajoxf.mongodb.net/?retryWrites=true&w=majority`;
+
+Connection(URL);
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+DefaultData();
